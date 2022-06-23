7034eab14d8097243d46867813e49f5797b13bfb
NiaMcAlkich <104858847+NiaMcAlkich@users.noreply.github.com>
2022-06-22 20:12:38 -0400

    Add files via upload

---
+++ PrintInitials/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class program
+    {
+
+        public static void printInitials()
+        {
+            Console.WriteLine("NNNNN   N   MMMMMM    MMMMMM");
+            Console.WriteLine("N   N   N   M    M    M    M");
+            Console.WriteLine("N   N   N   M    M    M    M");
+            Console.WriteLine("N   N   N   M    M    M    M");
+            Console.WriteLine("N   N   N   M    M    M    M");
+            Console.WriteLine("N   NNNNN   M    MMMMMM    M");
+            
+
+
+        }
+        static void Main(string[] args)
+        {
+            printInitials();
+        }
+    }
+}
